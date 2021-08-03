9bc329c2d753fac6f40baf1f607ce40c819f1973
blacktea20201231 <kaichun.yen@gmail.com>
2021-08-03 16:03:49 +0100

    hotspots 1. cluster garda station 1. finalize

---
+++ app/src/main/java/com/example/riderprotector/cluster/MyClusterItem.kt
@@ -0,0 +1,21 @@
+package com.example.riderprotector.cluster
+
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.clustering.ClusterItem
+
+class MyClusterItem(lat: Double, lng: Double, private val title: String, private val snippet: String) : ClusterItem {
+
+    private val position: LatLng = LatLng(lat, lng)
+
+    override fun getPosition(): LatLng {
+        return position
+    }
+
+    override fun getTitle(): String? {
+        return title
+    }
+
+    override fun getSnippet(): String? {
+        return snippet
+    }
+}
--- app/src/main/java/com/example/riderprotector/fragments/HotspotsMapFragment.kt
+++ app/src/main/java/com/example/riderprotector/fragments/HotspotsMapFragment.kt
@@ -12,24 +12,34 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.lifecycleScope
 import com.example.riderprotector.R
+import com.example.riderprotector.cluster.MyClusterItem
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.maps.android.clustering.Cluster
+import com.google.maps.android.clustering.ClusterManager
 import com.vmadalin.easypermissions.EasyPermissions
 import com.vmadalin.easypermissions.dialogs.SettingsDialog
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 
-class HotspotsMapFragment : Fragment(),OnMapReadyCallback , EasyPermissions.PermissionCallbacks{
+class HotspotsMapFragment : Fragment(),OnMapReadyCallback , EasyPermissions.PermissionCallbacks, ClusterManager.OnClusterClickListener<MyClusterItem>{
 
     private lateinit var googleMap: GoogleMap
     private lateinit var supportMapFragment: SupportMapFragment
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     private val dublin = LatLng(53.3498, -6.2603)
+    private var database = FirebaseDatabase.getInstance().getReference("hotspots")
+    private var clusterManager: ClusterManager<MyClusterItem>? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -40,7 +50,7 @@ class HotspotsMapFragment : Fragment(),OnMapReadyCallback , EasyPermissions.Perm
             requestLocationPermission(this)
         }
         //Initialize View
-        var view = inflater.inflate(R.layout.fragment_hotspots_map,container,false)
+        val view = inflater.inflate(R.layout.fragment_hotspots_map,container,false)
         //Initialize Map Fragment
         supportMapFragment = childFragmentManager.findFragmentById(R.id.hotspot_map) as SupportMapFragment
         //Async Map
@@ -67,10 +77,14 @@ class HotspotsMapFragment : Fragment(),OnMapReadyCallback , EasyPermissions.Perm
     }
 
     override fun onPermissionsGranted(requestCode: Int, perms: List<String>) {
-        Log.d("location_permission","permission granted!")
+        Log.d("location_permission","permission " +
+                "granted!")
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
+        this.googleMap = googleMap
+        //add item to cluster
+        setUpClusterer()
         //basic UI settings
         googleMap.uiSettings.apply {
             isZoomControlsEnabled = false // enlarge/shrink button
@@ -85,12 +99,59 @@ class HotspotsMapFragment : Fragment(),OnMapReadyCallback , EasyPermissions.Perm
         lifecycleScope.launch {
             //location, zoom level
             googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(dublin, ZOOM_LEVEL_MAP_START))
-            delay(2500)
+            delay(1500)
             googleMap.animateCamera(
-                CameraUpdateFactory.newLatLngZoom(dublin, ZOOM_LEVEL_DEFAULT),
+                CameraUpdateFactory.newLatLngZoom(dublin, 15f),
                 1000,
                 null
             )
         }
     }
+
+    private fun setUpClusterer() {
+        // Position the map.
+        googleMap.animateCamera(
+            CameraUpdateFactory.newLatLngZoom(
+                LatLng(53.3498, -6.2603),
+                ZOOM_LEVEL_DEFAULT
+            )
+        )
+
+        // Initialize the manager with the context and the map.
+        // (Activity extends context, so we can pass 'this' in the constructor.)
+        clusterManager = ClusterManager(context, googleMap)
+
+        // Point the map's listeners at the listeners implemented by the cluster manager.
+        googleMap.setOnCameraIdleListener(clusterManager)
+        googleMap.setOnMarkerClickListener(clusterManager)
+
+        // Add cluster items (markers) to the cluster manager.
+        addItems()
+    }
+
+    private fun addItems() {
+        database.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for (i in snapshot.children) {
+                    Log.d("Hotspots","Latitude:"+ i.child("coordinate").child("latitude").value.toString().toDouble())
+                    val offsetItem =
+                        MyClusterItem(
+                            i.child("coordinate").child("latitude").value.toString().toDouble(),
+                            i.child("coordinate").child("longitude").value.toString().toDouble(),
+                            i.child("details").child("title").value.toString(),
+                            i.child("details").child("brief").value.toString())
+                    clusterManager?.addItem(offsetItem)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.d("firebaseTesting", error.message)
+            }
+        })
+    }
+
+    override fun onClusterClick(cluster: Cluster<MyClusterItem>?): Boolean {
+        return true
+    }
+
 }
