4bb22867f4510c3870df31ca56bb8a7bc2e706bd
blacktea20201231 <kaichun.yen@gmail.com>
2021-08-06 12:19:07 +0100

    Hotspot map user_report_dialog

--- app/src/main/java/com/example/riderprotector/fragments/HotspotsMapFragment.kt
+++ app/src/main/java/com/example/riderprotector/fragments/HotspotsMapFragment.kt
@@ -1,6 +1,7 @@
 package com.example.riderprotector.fragments
 
 import android.Manifest
+import android.app.AlertDialog
 import android.content.pm.PackageManager
 import com.example.riderprotector.util.Constants.ZOOM_LEVEL_DEFAULT
 import com.example.riderprotector.util.Constants.ZOOM_LEVEL_MAP_START
@@ -12,8 +13,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.example.riderprotector.R
 import com.example.riderprotector.cluster.MyClusterItem
 import com.google.android.gms.location.FusedLocationProviderClient
@@ -22,6 +28,7 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -34,7 +41,10 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 
-class HotspotsMapFragment : Fragment(),OnMapReadyCallback , EasyPermissions.PermissionCallbacks, ClusterManager.OnClusterClickListener<MyClusterItem>{
+class HotspotsMapFragment : Fragment(), OnMapReadyCallback, EasyPermissions.PermissionCallbacks,
+    ClusterManager.OnClusterClickListener<MyClusterItem>, GoogleMap.OnMarkerClickListener,
+    GoogleMap.OnMapClickListener, GoogleMap.OnMapLongClickListener {
+
 
     private lateinit var googleMap: GoogleMap
     private lateinit var supportMapFragment: SupportMapFragment
@@ -42,19 +52,21 @@ class HotspotsMapFragment : Fragment(),OnMapReadyCallback , EasyPermissions.Perm
     private val dublin = LatLng(53.3498, -6.2603)
     private var database = FirebaseDatabase.getInstance().getReference("hotspots")
     private var clusterManager: ClusterManager<MyClusterItem>? = null
+    private lateinit var progressBar: ProgressBar
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         //request location permission, override onRequestPermission result
-        if (!hasLocationPermission(requireContext())){
+        if (!hasLocationPermission(requireContext())) {
             requestLocationPermission(this)
         }
         //Initialize View
-        val view = inflater.inflate(R.layout.fragment_hotspots_map,container,false)
+        val view = inflater.inflate(R.layout.fragment_hotspots_map, container, false)
         //Initialize Map Fragment
-        supportMapFragment = childFragmentManager.findFragmentById(R.id.hotspot_map) as SupportMapFragment
+        supportMapFragment =
+            childFragmentManager.findFragmentById(R.id.hotspot_map) as SupportMapFragment
         //Async Map
         supportMapFragment.getMapAsync(this)
         //return view
@@ -67,24 +79,25 @@ class HotspotsMapFragment : Fragment(),OnMapReadyCallback , EasyPermissions.Perm
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        EasyPermissions.onRequestPermissionsResult(requestCode,permissions,grantResults,this)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
     }
 
     override fun onPermissionsDenied(requestCode: Int, perms: List<String>) {
-        if (EasyPermissions.somePermissionPermanentlyDenied(this,perms)){
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
             SettingsDialog.Builder(requireActivity()).build().show()
-        }else{
+        } else {
             requestLocationPermission(this)
         }
     }
 
     override fun onPermissionsGranted(requestCode: Int, perms: List<String>) {
-        Log.d("location_permission","permission " +
-                "granted!")
+        Log.d(
+            "location_permission", "permission " +
+                    "granted!"
+        )
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
-
         //assign to google map var
         this.googleMap = googleMap
         //enable location
@@ -128,6 +141,8 @@ class HotspotsMapFragment : Fragment(),OnMapReadyCallback , EasyPermissions.Perm
                 null
             )
         }
+        googleMap.setOnMapClickListener(this)
+        googleMap.setOnMapLongClickListener(this)
     }
 
     private fun setUpClusterer() {
@@ -151,17 +166,22 @@ class HotspotsMapFragment : Fragment(),OnMapReadyCallback , EasyPermissions.Perm
         addItems()
     }
 
+    // add items onto the map, NOT　MARKERS!!!
     private fun addItems() {
         database.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 for (i in snapshot.children) {
-                    Log.d("Hotspots","Latitude:"+ i.child("coordinate").child("latitude").value.toString().toDouble())
+                    Log.d("Hotspots",
+                        "Latitude:" + i.child("coordinate").child("latitude").value.toString()
+                            .toDouble()
+                    )
                     val offsetItem =
                         MyClusterItem(
                             i.child("coordinate").child("latitude").value.toString().toDouble(),
                             i.child("coordinate").child("longitude").value.toString().toDouble(),
                             i.child("details").child("title").value.toString(),
-                            i.child("details").child("brief").value.toString())
+                            i.child("details").child("brief").value.toString()
+                        )
                     clusterManager?.addItem(offsetItem)
                 }
             }
@@ -175,5 +195,40 @@ class HotspotsMapFragment : Fragment(),OnMapReadyCallback , EasyPermissions.Perm
     override fun onClusterClick(cluster: Cluster<MyClusterItem>?): Boolean {
         return true
     }
+
+
+    override fun onMarkerClick(p0: Marker): Boolean {
+        return false
+    }
+
+    override fun onMapClick(p0: LatLng) {
+        Log.d("Hotpsots_map", "mapClicked $p0")
+    }
+
+    override fun onMapLongClick(p0: LatLng) {
+        Log.d("Hotpsots_map", "map Long Clicked $p0")
+        showReportDialog(p0)
+    }
+
+    private fun showReportDialog(p0: LatLng) {
+        val view = View.inflate(context, R.layout.report_dialog, null)
+        val builder = AlertDialog.Builder(context).setView(view)
+        val dialog = builder.create()
+        dialog.show()
+        dialog.window?.setBackgroundDrawableResource(android.R.color.white)
+        val title = view.findViewById<EditText>(R.id.report_title).text
+        val brief = view.findViewById<EditText>(R.id.report_brief).text
+
+        val reportBTN = view.findViewById<Button>(R.id.btn_report)
+        reportBTN.setOnClickListener{
+            //test for the value get from user
+            Log.d("Report","Title: $title")
+            Log.d("Report","Brief: $brief")
+        }
+
+
+
+    }
+
 
 }
